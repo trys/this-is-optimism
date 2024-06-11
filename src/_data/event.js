2d255e5493da08b2f30caa852e45382d82e80341
Trys Mudford <trys@clearleft.com>
2024-06-11 22:13:49 +0100

    feat: initial markup

---
+++ src/_data/event.js
@@ -0,0 +1,15 @@
+module.exports = {
+  date: {
+    name: 'Friday 20 Sep 2024',
+  },
+  location: {
+    name: 'Artelium Wine Estate, Sussex',
+  },
+  tickets: {
+    cost: '£389+VAT',
+    cta: {
+      link: '#',
+      label: 'Get notified'
+    }
+  }
+};
