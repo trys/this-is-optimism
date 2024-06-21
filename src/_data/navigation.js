2de5623d49e2fe9e836a71cb927b0f2f58718f5d
Trys Mudford <trys@clearleft.com>
2024-06-21 22:07:55 +0100

    feat: footer

--- src/_data/navigation.js
+++ src/_data/navigation.js
@@ -1,7 +1,14 @@
-module.exports = [
-  { label: 'Speakers', url: '/speakers' },
-  { label: 'Venue', url: '/venue' },
-  { label: 'Food', url: '#' },
-  { label: 'Why attend', url: '#' },
-  { label: 'Code of conduct', url: '/code-of-conduct', secondary: true },
-]
+module.exports = {
+  header: [
+    { label: 'Speakers', url: '/speakers' },
+    { label: 'Venue', url: '/venue' },
+    { label: 'Food', url: '#' },
+    { label: 'Why attend', url: '#' },
+    { label: 'Code of conduct', url: '/code-of-conduct', secondary: true },
+  ],
+  footer: [
+    { label: 'Code of conduct', url: '/code-of-conduct' },
+    { label: 'Reasons to attend', url: '#' },
+    { label: 'Privacy policy', url: '#' },
+  ]
+}
